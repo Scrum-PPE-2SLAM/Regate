9413d2dddd85f52299916eaa7ca648ebb9b7f49a
leojullerot <leojullerot@outlook.fr>
2018-03-20 09:53:22 +0100

    Add test java files

---
+++ regate-controller/src/main/java/fr/regate/project/App.java
@@ -0,0 +1,13 @@
+package fr.regate.project.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
