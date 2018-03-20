8e458d4a8c9c68193f110b9e6cbb6b6a9430e835
Thomas <thomas.galocha@hotmail.fr>
2018-03-20 13:33:41 +0100

    refactor view/ModifRegate.java

--- regate-view/src/main/java/fr/regate/project/view/ModifRegate.java
+++ regate-view/src/main/java/fr/regate/project/view/ModifRegate.java
@@ -9,34 +9,32 @@ public class ModifRegate extends JFrame
 
 	private static final long serialVersionUID = 1L;
 	private Window window;
-	private Bdd maBdd;
 	private JLabel lblSelRegate;
 	private JComboBox<String> cboSelRegate;
-	private AjoutRegate ajoutRegate;
+	private AjoutRegate addRegate;
 	
-	public ModifRegate(Window window, Bdd maBdd) 
+	public ModifRegate(Window window) 
 	{
 		this.window = window;
-		this.maBdd = maBdd;
 	}
 	
 	public void ajoutFenetre() 
 	{
-		this.ajoutRegate = new AjoutRegate(window, maBdd);
-		this.ajoutRegate.creationPanelAjoutRegate();
-		this.ajoutRegate.creationPanelParticipants();
-		this.ajoutRegate.creationPanelTitre("MODIFICATION REGATE");
+		this.addRegate = new AjoutRegate(window);
+		this.addRegate.creationPanelAjoutRegate();
+		this.addRegate.creationPanelParticipants();
+		this.addRegate.creationPanelTitre("MODIFICATION REGATE");
 	}
 	
 	public void ajoutCombo(String[] listeRegate) 
 	{
 		this.lblSelRegate = new JLabel("Selectionner la régate à modifier : ");
 		this.lblSelRegate.setBounds(175, 68, 380, 14);
-		this.ajoutRegate.panelTitreRegate.add(lblSelRegate);
+		this.addRegate.panelTitreRegate.add(lblSelRegate);
 		
 		this.cboSelRegate = new JComboBox<String>(listeRegate);
 		this.cboSelRegate.setBounds(383, 65, 161, 20);
-		this.ajoutRegate.panelTitreRegate.add(cboSelRegate);
+		this.addRegate.panelTitreRegate.add(cboSelRegate);
 	}
 	
 }
