e4dbc23cf82c6ded2e930a4acd61926947052505
leojullerot <leojullerot@outlook.fr>
2018-03-20 12:01:02 +0100

    Refactor package name

--- regate-view/src/main/java/fr/regate/project/view/Classement.java
+++ regate-view/src/main/java/fr/regate/project/view/Classement.java
@@ -1,3 +1,5 @@
+package fr.regate.project.view;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
--- regate-view/src/main/java/fr/regate/project/view/ModifRegate.java
+++ regate-view/src/main/java/fr/regate/project/view/ModifRegate.java
@@ -1,3 +1,5 @@
+package fr.regate.project.view;
+
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
